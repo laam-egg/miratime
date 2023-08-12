ad79e59dfc43552031899b53552c9a506dc75b9f
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-08-12 20:11:06 +0700

    Use another API backend

--- src/app/api/user/signup/route.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export async function POST(req: NextRequest) {
-    const { email, password } = await req.json();
-    return NextResponse.json({
-        email2: email, password2: password
-    });
-}
